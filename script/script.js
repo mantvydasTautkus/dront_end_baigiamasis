8b017dad636ef45c93c84bcdb0e8021db76294d9
Mantvydas Tautkus <mantvydastautkus@gmail.com>
2021-04-25 17:57:23 +0300

    pradedu mobaila tvarkyt

--- script/script.js
+++ script/script.js
@@ -9,3 +9,31 @@ const swiper = new Swiper('.swiper-container', {
         clickable: true
     },
 });
+
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
